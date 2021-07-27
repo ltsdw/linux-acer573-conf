093e6aad3a2e75cc824b401a52f11e388f8b3803
ltsdw <44977415+ltsdw@users.noreply.github.com>
2021-07-27 08:55:03 -0300

    scripts

---
+++ scripts/aux-script.py
@@ -0,0 +1,129 @@
+#! /bin/python
+
+from typing import List
+
+class PKGBUILD:
+    def __init__(self, _major: float, _minor: int) -> None:
+        from sys import exit
+        from os.path import exists, join, abspath
+        from os import getcwd, chdir
+
+        chdir(abspath(join(getcwd(), '..')))
+
+        if not exists('PKGBUILD'):
+            exit('No PKGBUILD found!')
+
+        self._major: float = _major
+        self._minor: int = _minor
+        self.pkgver: str = f'{self._major}.{self._minor}'
+        self.linux_url: str
+        self.sign_url: str
+        self.patch_url: str
+        self.source: str
+        self.sha256sums: str = ''
+
+    def returnSource(self, directories: List[str]) -> str:
+        from os import walk
+
+        source: str = '''        "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-${_major}.tar.xz"
+        "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-${_major}.tar.sign"
+        "https://cdn.kernel.org/pub/linux/kernel/v5.x/patch-${pkgver}.xz"\n'''
+
+        for directory in directories:
+            for (_, _, files) in walk(directory):
+                for file in files:
+                    source += f'        "{file}"\n'
+
+        return source
+
+    def returnSha256(self, pwd: str, directories: List[str], list_of_filenames: List[str]) -> str:
+        from hashlib import sha256
+        from os import walk, chdir
+
+        sha256sums: str = ''
+        for filename in list_of_filenames:
+            with open(filename, 'rb') as f_linux:
+                _bytes: bytes = f_linux.read()
+                sha256sum: str = sha256(_bytes).hexdigest()
+                sha256sums += f'            "{sha256sum}"\n'
+
+        for directory in directories:
+            chdir(pwd + '/' + directory)
+            directory: str = pwd + '/' + directory
+            for (_, _, filenames) in walk(directory):
+                for filename in filenames:
+                    if 'config-custom-sdw' in filename:
+                        sha256sums += f'            "SKIP"\n'
+                    else:
+                        with open(filename, 'rb') as f_linux:
+                            _bytes: bytes = f_linux.read()
+                            sha256sum: str = sha256(_bytes).hexdigest()
+                            sha256sums += f'            "{sha256sum}"\n'
+        chdir(pwd)
+
+        return sha256sums
+
+    def download(self) -> None:
+        from requests import get, Response
+        from os.path import exists
+        from os import getcwd
+
+        linux: str = f'linux-{self._major}.tar.xz'
+        sign: str = f'linux-{self._major}.tar.sign'
+        patch: str = f'patch-{self.pkgver}.xz'
+
+        self.linux_url: str = f'https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-{self._major}.tar.xz'
+        self.sign_url: str = f'https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-{self._major}.tar.sign'
+        self.patch_url: str = f'https://cdn.kernel.org/pub/linux/kernel/v5.x/patch-{self.pkgver}.xz'
+
+        if not exists(linux):
+            print(f'Downloading {linux}')
+            linux_file: Response = get(self.linux_url, allow_redirects=True)
+            open(linux, 'wb').write(linux_file.content)
+
+        if not exists(sign):
+            print(f'Downloading {sign}')
+            sign_file: Response = get(self.sign_url, allow_redirects=True)
+            open(sign, 'wb').write(sign_file.content)
+ 
+        if not exists(patch):
+            print(f'Downloading {patch}')
+            patch_file: Response = get(self.patch_url, allow_redirects=True)
+            open(patch, 'wb').write(patch_file.content)
+            
+        self.sha256sums = self.returnSha256(getcwd(), ['patches', 'cpu_scheduler', 'config'],
+                                            [linux, sign, patch])
+
+    def updatePakageBuild(self) -> None:
+        is_open: bool = False
+        self.source = self.returnSource(['patches', 'cpu_scheduler', 'config'])
+
+        with open('PKGBUILD', 'r') as pkgbuild:
+            tmp: List[str] = pkgbuild.readlines()
+
+        with open('PKGBUILD', 'w') as pkgbuild:
+            for line in tmp:
+                if '_major=' in line:
+                    pkgbuild.write(f'_major={self._major}\n')
+                elif '_minor=' in line:
+                    pkgbuild.write(f'_minor={self._minor}\n')
+                elif 'source=(' in line:
+                    is_open = True
+                    pkgbuild.write(line)
+                    pkgbuild.write(self.source)
+                elif 'sha256sums=(' in line:
+                    is_open = True
+                    pkgbuild.write(line)
+                    pkgbuild.write(self.sha256sums)
+                elif '        )' in line or '       )' in line:
+                    is_open = False
+                    pkgbuild.write(line)
+                elif not is_open:
+                    pkgbuild.write(line)
+
+if __name__ == '__main__':
+
+    re_pkgbuild = PKGBUILD(5.13, 5)
+    re_pkgbuild.download()
+    re_pkgbuild.updatePakageBuild()
+
